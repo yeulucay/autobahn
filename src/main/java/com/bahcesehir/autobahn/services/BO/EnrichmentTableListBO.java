2d92ccda99b77784bcf3a97d5aae7a48fe561076
Yunus Emre Ulucay <ye.ulucay@gmail.com>
2018-12-27 23:57:14 -0500

    table list BO reorganization

---
+++ src/main/java/com/bahcesehir/autobahn/services/BO/EnrichmentTableListBO.java
@@ -0,0 +1,75 @@
+package com.bahcesehir.autobahn.services.BO;
+
+import com.bahcesehir.autobahn.controllers.views.EnrichmentSourceView;
+import com.bahcesehir.autobahn.controllers.views.EnrichmentTableNamesView;
+import com.bahcesehir.autobahn.repositories.EnrichmentSourceRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class EnrichmentTableListBO implements BaseBO<EnrichmentTableNamesView> {
+
+    private EnrichmentSourceRepository repository;
+    private Long enrichmentSourceId;
+
+    public EnrichmentTableListBO(Long enrichmentSourceId, EnrichmentSourceRepository repository) {
+        this.enrichmentSourceId = enrichmentSourceId;
+        this.repository = repository;
+    }
+
+    @Override
+    public EnrichmentTableNamesView execute() {
+        EnrichmentSourceByIdBO bo = new EnrichmentSourceByIdBO(enrichmentSourceId, repository);
+        EnrichmentSourceView enrichmentSource = bo.execute();
+
+        if(!Objects.isNull(enrichmentSource)){
+            //TODO: Throw exception
+        }
+
+        String jdbcType = "";
+        switch (enrichmentSource.getEnrichmentSourceTypeCode()){
+            case "MYSQL":
+                jdbcType = "mysql";
+                break;
+            case "POSTGRESQL":
+                jdbcType = "postgresql";
+                break;
+        }
+
+        String url = String.format("jdbc:%s://%s:%s/%s",
+                jdbcType,
+                enrichmentSource.getAddress(),
+                enrichmentSource.getPort(),
+                enrichmentSource.getDatabaseName());
+
+        try {
+            Connection conn = DriverManager.getConnection(url,
+                    enrichmentSource.getUsername(),enrichmentSource.getPassword());
+            ResultSet rs = conn.getMetaData()
+                    .getTables(enrichmentSource.getDatabaseName(), "public", "%", null);
+            List<String> results = new ArrayList<>();
+            while(rs.next()){
+                if(rs.getString(4).equals("TABLE")) {
+                    results.add(rs.getString(3));
+                }
+            }
+            return new EnrichmentTableNamesView(results);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void validate() {
+
+    }
+}
--- src/main/java/com/bahcesehir/autobahn/services/impl/MetaServiceImpl.java
+++ src/main/java/com/bahcesehir/autobahn/services/impl/MetaServiceImpl.java
@@ -4,6 +4,7 @@ import com.bahcesehir.autobahn.controllers.views.EnrichmentSourceView;
 import com.bahcesehir.autobahn.controllers.views.EnrichmentTableNamesView;
 import com.bahcesehir.autobahn.repositories.EnrichmentSourceRepository;
 import com.bahcesehir.autobahn.services.BO.EnrichmentSourceByIdBO;
+import com.bahcesehir.autobahn.services.BO.EnrichmentTableListBO;
 import com.bahcesehir.autobahn.services.MetaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,47 +30,7 @@ public class MetaServiceImpl implements MetaService {
 
     @Override
     public EnrichmentTableNamesView getEnrichmentTableNames(Long enrichmentSourceId) {
-        EnrichmentSourceByIdBO bo = new EnrichmentSourceByIdBO(enrichmentSourceId, enrichmentSourceRepository);
-        EnrichmentSourceView enrichmentSource = bo.execute();
-
-        if(!Objects.isNull(enrichmentSource)){
-            //TODO: Throw exception
-        }
-
-        String jdbcType = "";
-        switch (enrichmentSource.getEnrichmentSourceTypeCode()){
-            case "MYSQL":
-                jdbcType = "mysql";
-                break;
-            case "POSTGRESQL":
-                jdbcType = "postgresql";
-                break;
-        }
-
-        String url = String.format("jdbc:%s://%s:%s/%s",
-                jdbcType,
-                enrichmentSource.getAddress(),
-                enrichmentSource.getPort(),
-                enrichmentSource.getDatabaseName());
-
-        try {
-            Connection conn = DriverManager.getConnection(url,
-                    enrichmentSource.getUsername(),enrichmentSource.getPassword());
-            ResultSet rs = conn.getMetaData()
-                    .getTables(enrichmentSource.getDatabaseName(), "public", "%", null);
-            List<String> results = new ArrayList<>();
-            while(rs.next()){
-                if(rs.getString(4).equals("TABLE")) {
-                    results.add(rs.getString(3));
-                }
-            }
-            return new EnrichmentTableNamesView(results);
-
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-
+        EnrichmentTableListBO bo = new EnrichmentTableListBO(enrichmentSourceId, enrichmentSourceRepository);
+        return bo.execute();
     }
 }
