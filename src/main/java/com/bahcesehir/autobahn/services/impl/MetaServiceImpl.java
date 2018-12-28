21b1c7ee67b7e712438318839ba2f2922f322b14
Yunus Emre Ulucay <ye.ulucay@gmail.com>
2018-12-27 23:58:30 -0500

    table list BO reorganization

--- src/main/java/com/bahcesehir/autobahn/services/impl/MetaServiceImpl.java
+++ src/main/java/com/bahcesehir/autobahn/services/impl/MetaServiceImpl.java
@@ -1,21 +1,11 @@
 package com.bahcesehir.autobahn.services.impl;
 
-import com.bahcesehir.autobahn.controllers.views.EnrichmentSourceView;
 import com.bahcesehir.autobahn.controllers.views.EnrichmentTableNamesView;
 import com.bahcesehir.autobahn.repositories.EnrichmentSourceRepository;
-import com.bahcesehir.autobahn.services.BO.EnrichmentSourceByIdBO;
 import com.bahcesehir.autobahn.services.BO.EnrichmentTableListBO;
 import com.bahcesehir.autobahn.services.MetaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 
 @Service
